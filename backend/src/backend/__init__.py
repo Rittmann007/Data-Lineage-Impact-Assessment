dc849715411087da1f7ecb73274d96681f935d33
Ritom Paul <rittmann007@gmail.com>
2026-09-17 00:36:52 +0530

    database setup

--- backend/src/backend/__init__.py
+++ backend/src/backend/__init__.py
@@ -1,2 +1,31 @@
-def main() -> None:
-    print("Hello from backend!")
+from dotenv import load_dotenv
+load_dotenv()
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+import os
+
+
+uri = os.environ.get("MONGO_URI")
+client = MongoClient(uri, server_api=ServerApi("1"))
+
+@asynccontextmanager # this section will run before the app actually starts
+async def lifespan(app: FastAPI):
+    app.state.mongo_client = client # store the Mongo client on the app during lifespan, then read it inside the controller.
+    try:
+        client.admin.command("ping")
+        print("Pinged your deployment. You successfully connected to MongoDB!", flush=True)
+    except Exception as e:
+        print(e, flush=True)
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+
+#routes
+
+# root
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
